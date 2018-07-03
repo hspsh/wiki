e41a23dda8d31a747878a2aa0cff24eb12b17289
Norbert Szulc <norbert@not7cd.net>
2018-07-03 17:47:31 +0200

    Update userscript.js
    As proposed by @justdzasta

--- conf/userscript.js
+++ conf/userscript.js
@@ -8,15 +8,13 @@ fetch("https://at.hs3.pl/api/now")
       .querySelector("ul.nav.navbar-nav")
       .querySelectorAll("li.level1 > a")[2];
 
-    whoisbtn.innerHTML = "ktohakuje";
-
+    whoisbtn.innerHTML = `ktohakuje&nbsp;<span class="label label-default">${data.headcount}</span>`
     if (data.headcount > 0) {
-      whoisbtn.innerHTML += `&nbsp;<span class="label label-success">${
-        data.headcount
-      }&nbsp;hacker${data.headcount > 1 ? "s" : ""}</span>`;
+      whoisbtn.innerHTML += `&nbsp;<span class="label label-success">open</span>`;
     } else {
       whoisbtn.innerHTML += `&nbsp;<span class="label label-danger">closed</span>`;
     }
+  ,
   })
   .catch(function (error) {
     console.log(error)
